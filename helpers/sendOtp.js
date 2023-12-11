ffb1023ad8efb38c442884e9d5779df27fca2166
Laxya Rupeja <121308243+LaxyaRupeja@users.noreply.github.com>
2023-12-11 16:09:59 +0530

    Update sendOtp.js

--- helpers/sendOtp.js
+++ helpers/sendOtp.js
@@ -2,19 +2,7 @@ const nodemailer = require("nodemailer");
 require("dotenv").config();
 
 const sendOtpEmail = (email,otp,transporter) => {
-    const mailOptions = {
-        from:process.env.GMAIL,
-        to:email,
-        subject:"OTP Verification Chrome Extension",
-        text:`Your OTP is ${otp}`
-    }
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-          console.error(error);
-        } else {
-          console.log('Email sent: ' + info.response);
-        }
-      });
+   
 }
 
 module.exports = sendOtpEmail
