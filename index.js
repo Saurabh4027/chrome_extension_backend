70c61bc537cb5525ac2c86f75e1b7406e553c2f7
Laxya Rupeja <121308243+LaxyaRupeja@users.noreply.github.com>
2023-12-11 16:10:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,19 @@ app.post('/validate-email', async (req, res) => {
       return res.status(400).json({ error: 'Invalid email address' });
     }
     const otp = generateAlphanumericCode(6);
-    sendOtpEmail(email, otp,transporter);
+    const mailOptions = {
+        from:process.env.GMAIL,
+        to:email,
+        subject:"OTP Verification Chrome Extension",
+        text:`Your OTP is ${otp}`
+    }
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          console.error(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+        });
     await Otp.findOneAndUpdate({ email }, { email,otp }, { upsert: true });
     res.json({ success: true, message: 'OTP sent successfully' });
 });
