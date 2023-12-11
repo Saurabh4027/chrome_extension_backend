e457de38f5bc89837ad4bfc2a75af57bad052547
Saurabh4027 <144934882+Saurabh4027@users.noreply.github.com>
2023-12-11 11:53:55 +0530

    Update sendOtp.js

--- helpers/sendOtp.js
+++ helpers/sendOtp.js
@@ -3,7 +3,9 @@ require("dotenv").config();
 
 const sendOtpEmail = (email,otp) => {
     const transporter = nodemailer.createTransport({
-        service:"gmail",
+        host:"smtp.gmail.com",
+        port : 587,
+        secure : false,
         auth:{
             user:process.env.GMAIL,
             pass:process.env.GMAIL_PASSWORD
